3e02df2ada2c979bac4cabc0e9e7c7b02426fad7
dacferreira <diogoa42@gmail.com>
2016-10-06 00:26:04 -0300

    primeiro check in

---
+++ js/app.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJLwHSz8RmS9HD01SS7x8TeI93wk1SGDE",
+    authDomain: "curso-fb-3b707.firebaseapp.com",
+    databaseURL: "https://curso-fb-3b707.firebaseio.com/",
+    storageBucket: "curso-fb-3b707.appspot.com",
+    messagingSenderId: "167299847610"
+  };
+  firebase.initializeApp(config);
