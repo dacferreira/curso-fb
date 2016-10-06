35b7b897941d426e95237225bd5b65b85155890b
dacferreira <diogoa42@gmail.com>
2016-10-06 14:35:00 -0300

    inclusão dos arquivos de autenticação

---
+++ js/authentication.js
@@ -0,0 +1,15 @@
+var authFacebookButton = document.getElementById('authFacebookButton');
+var authTwitterButton = document.getElementById('authTwitterButton');
+var authGoogleButton = document.getElementById('authGoogleButton');
+var authGitHubButton = document.getElementById('authGitHubButton');
+var authAnonymouslyButton = document.getElementById('authAnonymouslyButton');
+var createUserButton = document.getElementById('createUserButton');
+var logonButton = document.getElementById('logonButton');
+
+
+//inputs
+var emailInput = documento.getElementById('emailInput');
+var passwordInput = documento.getElementById('passwordInput');
+
+//Displays
+var displayName = document.getElementById('displayName');
